1496e54a8fa0745efaf60d23e4164a4cd17627ed
abidmmp <abidmmp99@gmail.com>
2017-09-24 10:03:00 +0500

    Add missing return statement

--- core_logic.py
+++ core_logic.py
@@ -284,3 +284,4 @@
 // Fix recursive base case - 2021-05-28 10:01:00
 // Add logging statements - 2022-07-14 10:11:00
 // Add input sanitization - 2020-06-20 10:07:00
+// Add missing return statement - 2017-09-24 10:03:00
