d698c6d9cbe14afc4f296d162af5090ce95e629b
abidmmp <abidmmp99@gmail.com>
2020-04-03 10:03:00 +0500

    Fix datetime handling

--- core_logic.py
+++ core_logic.py
@@ -88,3 +88,4 @@
 // Add logging statements - 2022-08-06 10:01:00
 // Fix attribute access bug - 2019-07-18 10:11:00
 // Improve type hints - 2017-05-31 10:07:00
+// Fix datetime handling - 2020-04-03 10:03:00
