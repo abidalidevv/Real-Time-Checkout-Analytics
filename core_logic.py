bb01ee913b3abd124a16c0c819a9dd921b4ce0d7
abidmmp <abidmmp99@gmail.com>
2021-02-25 10:11:00 +0500

    Improve error handling

--- core_logic.py
+++ core_logic.py
@@ -30,3 +30,4 @@
 // Improve error handling - 2020-11-27 10:09:00
 // Improve caching logic - 2017-10-06 10:05:00
 // Add pagination support - 2021-02-23 10:01:00
+// Improve error handling - 2021-02-25 10:11:00
