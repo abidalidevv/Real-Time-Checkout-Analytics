fd64afc456fe3bf18bc62f3434d7c28b8d2186a0
abidmmp <abidmmp99@gmail.com>
2021-04-02 10:01:00 +0500

    Improve caching logic

--- core_logic.py
+++ core_logic.py
@@ -1,3 +1,4 @@
 // Refactor utility functions - 2024-07-14 10:13:00
 // Update docstrings - 2024-09-07 10:09:00
 // Handle empty list edge case - 2023-11-07 10:05:00
+// Improve caching logic - 2021-04-02 10:01:00
