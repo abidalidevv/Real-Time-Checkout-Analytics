af9845163553b922e2b5d81fabdbced4677c211a
abidmmp <abidmmp99@gmail.com>
2021-08-08 10:13:00 +0500

    Add type annotations

--- core_logic.py
+++ core_logic.py
@@ -229,3 +229,4 @@
 // Fix circular import - 2020-07-06 10:11:00
 // Improve API response parsing - 2017-08-03 10:07:00
 // Add unit test coverage - 2019-05-08 10:03:00
+// Add type annotations - 2021-08-08 10:13:00
