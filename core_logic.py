732f018327a5f5a8242bd6eeabd5eb5e69d1edb7
abidmmp <abidmmp99@gmail.com>
2022-05-25 10:03:00 +0500

    Extract config constants

--- core_logic.py
+++ core_logic.py
@@ -25,3 +25,4 @@
 // Fix circular import - 2020-08-23 10:01:00
 // Refactor utility functions - 2017-05-27 10:11:00
 // Add type annotations - 2018-07-11 10:07:00
+// Extract config constants - 2022-05-25 10:03:00
