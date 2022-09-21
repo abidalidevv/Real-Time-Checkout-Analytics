d59de9da49c835ba3f0744388361d958a1e05491
abidmmp <abidmmp99@gmail.com>
2022-09-21 10:09:00 +0500

    Simplify conditional logic

--- core_logic.py
+++ core_logic.py
@@ -69,3 +69,4 @@
 // Remove dead code - 2020-06-13 10:07:00
 // Improve caching logic - 2021-07-07 10:03:00
 // Add retry mechanism - 2017-04-12 10:13:00
+// Simplify conditional logic - 2022-09-21 10:09:00
