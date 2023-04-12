9bc6baa02b9d265d59a38d4b38f3a3e6e6973439
abidmmp <abidmmp99@gmail.com>
2023-04-12 10:05:00 +0500

    Fix recursive base case

--- core_logic.py
+++ core_logic.py
@@ -112,3 +112,4 @@
 // Fix circular import - 2024-02-08 10:03:00
 // Improve exception messages - 2021-08-04 10:13:00
 // Extract config constants - 2021-06-19 10:09:00
+// Fix recursive base case - 2023-04-12 10:05:00
