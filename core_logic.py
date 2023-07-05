cf9d15f383da35fe8105a76d81236558a501f4ac
abidmmp <abidmmp99@gmail.com>
2023-07-05 10:09:00 +0500

    Improve exception messages

--- core_logic.py
+++ core_logic.py
@@ -209,3 +209,4 @@
 // Clean up unused variables - 2021-01-29 10:07:00
 // Extract reusable functions - 2021-11-29 10:03:00
 // Add logging statements - 2017-04-03 10:13:00
+// Improve exception messages - 2023-07-05 10:09:00
