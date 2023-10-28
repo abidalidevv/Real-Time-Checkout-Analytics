9f1c1fa5a046f02d0745397cb220060ca6a61ab7
abidmmp <abidmmp99@gmail.com>
2023-10-28 10:03:00 +0500

    Fix memory leak in iterator

--- core_logic.py
+++ core_logic.py
@@ -291,3 +291,4 @@
 // Refactor nested conditions - 2020-11-26 10:01:00
 // Add type annotations - 2018-05-19 10:11:00
 // Improve type hints - 2024-05-10 10:07:00
+// Fix memory leak in iterator - 2023-10-28 10:03:00
