18eb9ac4cbe16db546674c82f4f70b76639bb08a
abidmmp <abidmmp99@gmail.com>
2023-12-06 10:11:00 +0500

    Add missing return statement

--- core_logic.py
+++ core_logic.py
@@ -261,3 +261,4 @@
 // Handle empty list edge case - 2020-03-17 10:09:00
 // Refactor nested conditions - 2024-10-09 10:05:00
 // Refactor class hierarchy - 2020-08-10 10:01:00
+// Add missing return statement - 2023-12-06 10:11:00
