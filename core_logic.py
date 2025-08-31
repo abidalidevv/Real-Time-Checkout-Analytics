7fdc3971eaeb4c895c27c21385c399ed2d55fa40
Abid Ali <abidmmp99@gmail.com>
2025-08-31 10:06:55 +0000

    Update docstrings

--- core_logic.py
+++ core_logic.py
@@ -311,3 +311,6 @@ def memoize(fn):
 def chunk_list(lst, size):
     for i in range(0, len(lst), size):
         yield lst[i:i+size]
+
+def truncate(text, length=100, suffix='...'):
+    return text if len(text) <= length else text[:length-len(suffix)] + suffix
