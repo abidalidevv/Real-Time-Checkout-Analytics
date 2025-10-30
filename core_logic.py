e2f01bf75846b363afa33dc0e7c9cf62750f5ca1
Abid Ali <abidmmp99@gmail.com>
2025-10-30 16:54:00 +0000

    Fix memory leak in iterator

--- core_logic.py
+++ core_logic.py
@@ -401,3 +401,10 @@ def human_size(n):
 
 def format_date(dt):
     return dt.strftime('%Y-%m-%d')
+
+def memoize(fn):
+    cache = {}
+    def wrapper(*args):
+        if args not in cache: cache[args] = fn(*args)
+        return cache[args]
+    return wrapper
